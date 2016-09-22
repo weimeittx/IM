b00c529253db5c89c55a47f9cb24616261269118
weimeittx <422450455@qq.com>
2016-09-22 20:36:10 +0800

    2016年9月22日 00:33:46

--- data/src/main/java/cn/dunn/service/FileNote.java
+++ data/src/main/java/cn/dunn/service/FileNote.java
@@ -1,53 +1,8 @@
 package cn.dunn.service;
 
-import java.io.InputStream;
-import java.util.Date;
-import java.util.Map;
-
 /**
  * Created by Administrator on 2016/9/20.
  */
-public interface FileNote<ID> {
-    String CREATE_TIME = "createTime";
-    String UPDATE_TIME = "updateTime";
-    String TYPE = "type";
-    String POSTFIX = "postfix";
-
+public interface FIleNote<ID> {
     ID getId();
-
-    default String MD5() {
-        return null;
-    }
-
-    default long length() {
-        return 0;
-    }
-
-    default String postfix() {
-        return null;
-    }
-
-    default String type() {
-        return null;
-    }
-
-    default Date createTime() {
-        return null;
-    }
-
-    default Date updateTime() {
-        return null;
-    }
-
-    default InputStream inputStream() {
-        return null;
-    }
-
-    default Map<String, String> metaData() {
-        return null;
-    }
-
-    default String filename() {
-        return null;
-    }
 }
